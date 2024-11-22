8b1dd21759674067b9b5b9f67dd7a98abe88981b
SamFabra <samfabra5@gmail.com>
2024-11-22 11:24:19 -0500

    end game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,5 +25,13 @@ public class GameManager : MonoBehaviour
         Debug.Log("Spawning zombie");
         Vector3 spawnPosition = spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position;
         _zombieSpawner.SpawnEnemy(spawnPosition);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.CompareTag("Zombie"))
+        {
+            Debug.Log("End game");
+        }
     }
 }
